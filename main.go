3c513810956b15ee33bc1d5d325466a30b8e2eb7
youucho <wjsgus1224@gmail.com>
2025-08-09 17:02:53 +0900

    merge two aul2 files

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
+	"maps"
 	"os"
 	"strings"
 )
 
-type aul2 struct {
-}
+type aul2 map[string]map[string]string
 
 func error(e any) { // why error isnt type
 	if e != nil {
@@ -15,9 +15,9 @@ func error(e any) { // why error isnt type
 	}
 }
 
-func ParseAul2(file string) map[string][][2]string {
+func ParseAul2(file string) aul2 {
 	array := strings.Split(strings.ReplaceAll(file, "\r\n", "\n"), "\n") // i hate windows
-	aul2 := make(map[string][][2]string)
+	result := make(aul2)
 	currentField := ""
 	for _, v := range array {
 		if len(v) == 0 {
@@ -28,24 +28,50 @@ func ParseAul2(file string) map[string][][2]string {
 			continue
 		case '[':
 			currentField = v[1 : len(v)-1]
-			fmt.Println(currentField)
+			result[currentField] = make(map[string]string)
 		default:
 			if currentField == "" {
-				return map[string][][2]string{} // error
+				return make(aul2) // error
 			} else {
 				original, translation, found := strings.Cut(v, "=")
 				if !found {
 					continue
 				}
-				aul2[currentField] = append(aul2[currentField], [2]string{original, translation})
+				result[currentField][original] = translation
 			}
 		}
 	}
-	return aul2
+	return result
+}
+
+func MergeAul2(base aul2, sub aul2) aul2 {
+	result := make(aul2)
+	maps.Copy(result, base)
+	for field, v := range sub {
+		if result[field] == nil {
+			result[field] = make(map[string]string)
+		}
+		for ori, tra := range v {
+			if _, prs := base[field][ori]; prs {
+				fmt.Println("skip cuz exist")
+				continue
+			} else {
+				result[field][ori] = tra
+			}
+		}
+	}
+	return result
 }
 
 func main() {
-	test, err := os.ReadFile("English.aul2")
+	base, err := os.ReadFile("base.aul2")
 	error(err)
-	fmt.Println(ParseAul2(string(test)))
+	sub, err := os.ReadFile("sub.aul2")
+	error(err)
+	baseaul2 := ParseAul2(string(base))
+	subaul2 := ParseAul2(string(sub))
+	fmt.Println(baseaul2)
+	fmt.Println(subaul2)
+	result := MergeAul2(baseaul2, subaul2)
+	fmt.Println(result)
 }
